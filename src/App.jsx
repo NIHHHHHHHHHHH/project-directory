725dddf5eff58e27dbe3e525e1578d49e5254705
NIHHHHHHHHHHH <nihalgavandi00@gmail.com>
2026-02-21 13:41:01 +0530

    chore: initialize project with Vite, React, Tailwind Css v4

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+      <div>
+      <h1 className='text-center my-20 text-5xl text-blue-300'>Project Setup using React + Vite + tailwindcss</h1>
+      </div>
+  )
+}
+
+export default App
